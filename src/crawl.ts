b6dc34878d378d9e8aa7105aa3124f09692f3153
Ingrid Kamga <xingridkamga@gmail.com>
2024-08-15 08:04:15 +0100

    Extend saved cookies

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,43 +1,69 @@
-import puppeteer, { Page } from 'puppeteer'
+import puppeteer, { Browser } from 'puppeteer'
 import { State, Store } from './types'
 
-// Retrieve current state
+/**
+ * Retrieve current state
+ */
 export const crawl = async (store: Store): Promise<State> => {
   // Launch the browser and open a new blank page
+
   const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  page.setDefaultTimeout(0)
+
+  // Prepare pages
+
+  const [page] = await browser.pages()
+  const connectPage = await browser.newPage()
+
+  await disableTimeout(browser)
 
   // Restore cookies
-  await loadCookies(store, page)
+
+  await loadCookies(store, browser)
+
+  // Science connect
+
+  await connectPage.goto('https://wiley.scienceconnect.io/dashboard', {
+    waitUntil: 'networkidle2',
+  })
+
+  await connectPage.waitForSelector(
+    'xpath///h2[text()="Institutional Memberships"]',
+  )
 
   // Go to entry
+
   await page.goto(store.entry, { waitUntil: 'networkidle2' })
   await page.waitForSelector('xpath///h1[text()="My Submissions"]')
 
   // Persist cookies
-  await persistCookies(store, page)
+
+  await persistCookies(store, browser)
 
   // Fetch data
+
   await page.goto(store.tracker, { waitUntil: 'networkidle2' })
   const handle = await page.locator('body pre').waitHandle()
   const response = await handle.evaluate((el) => el.textContent)
 
   // Close browser
+
   await browser.close()
 
   // Throw error if no response
+
   if (!response) {
     throw Error('Could not scrape data')
   }
 
   // Collect data
+
   const json = JSON.parse(response)
   const data = json.hits
     .map((e: any) => e.actualStage.data)
     .find((e: any) => e.registrySubmissionCode === store.submissionId)
 
   // Return state
+
   return {
     ref: store.submissionId,
     title: data.name,
@@ -47,16 +73,32 @@ export const crawl = async (store: Store): Promise<State> => {
   }
 }
 
-const loadCookies = async (store: Store, page: Page) => {
+const disableTimeout = async (browser: Browser) => {
+  for (const page of await browser.pages()) {
+    page.setDefaultTimeout(0)
+  }
+}
+
+const loadCookies = async (store: Store, browser: Browser) => {
   if (!Array.isArray(store.cookies)) {
     return
   }
 
   console.log(`${store._id}: loading cookies...`)
-  await page.setCookie(...store.cookies)
+
+  for (const page of await browser.pages()) {
+    await page.setCookie(...store.cookies)
+  }
+
   console.log(`${store._id}: loaded cookies`)
 }
 
-const persistCookies = async (store: Store, page: Page) => {
-  store.cookies = await page.cookies()
+const persistCookies = async (store: Store, browser: Browser) => {
+  let cookies = []
+
+  for (const page of await browser.pages()) {
+    cookies.push(...await page.cookies())
+  }
+
+  store.cookies = cookies
 }
